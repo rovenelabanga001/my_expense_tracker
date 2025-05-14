ece54467716a14a36e4ae0dceb926f725f18765d
rovenelabanga <rovenelabanga2001@gmail.com>
2025-05-14 13:05:09 +0300

    adds an options request to the signup backend endpoint

--- server/app.py
+++ server/app.py
@@ -61,7 +61,7 @@ class Signup(Resource):
         if not all([firstname, lastname, email, password]):
             return {"error" : "All fields are required"}, 400
 
-        existing_user = User.query.filter_by(email=email).first
+        existing_user = User.query.filter_by(email=email).first()
         if existing_user:
             return {"error": "A user with that email already exists"}, 409
         
@@ -82,7 +82,7 @@ class Signup(Resource):
 
         return make_response(user_data, 201)
 
-api.add_resource(Signup, "/signup")
+api.add_resource(Signup, "/signup", methods=["POST", "OPTIONS"])
 
 #signin
 class Signin(Resource):
